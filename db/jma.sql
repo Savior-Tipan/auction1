0655e60a2b2f8b074932411862c07547b132f89f
Ryan Manalo <21-34023@g.batstate-u.edu.ph>
2024-10-04 20:15:56 +0800

    update on sql database, index ni user at user login

--- db/jma.sql
+++ db/jma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2024 at 11:34 AM
+-- Generation Time: Oct 04, 2024 at 02:14 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -113,7 +113,8 @@ INSERT INTO `categories` (`category_id`, `category_name`, `created_at`) VALUES
 (15, 'Trailer Head', '2024-09-29 15:17:03'),
 (16, 'Flat Trucks', '2024-09-29 15:17:03'),
 (17, 'Tankers', '2024-09-29 15:17:03'),
-(18, 'Used Trucks', '2024-09-29 15:17:03');
+(18, 'Used Trucks', '2024-09-29 15:17:03'),
+(19, 'Dump Truck', '2024-10-01 13:59:57');
 
 -- --------------------------------------------------------
 
@@ -131,17 +132,14 @@ CREATE TABLE `transactions` (
   `payment_status` enum('pending','completed','failed') DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `truckcategories`
+-- Dumping data for table `transactions`
 --
 
-CREATE TABLE `truckcategories` (
-  `category_id` int(11) NOT NULL,
-  `category_name` varchar(100) NOT NULL,
-  `category_description` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `transactions` (`transaction_id`, `truck_id`, `buyer_id`, `final_bid`, `transaction_time`, `payment_method`, `payment_status`) VALUES
+(1, 1, 1, 50000.00, '2024-09-20 21:53:54', 'cash', 'completed'),
+(2, 2, 2, 62000.00, '2024-09-20 21:53:54', 'bank_transfer', 'completed'),
+(3, 3, 3, 55000.00, '2024-09-20 21:53:54', 'financing', 'pending');
 
 -- --------------------------------------------------------
 
@@ -172,26 +170,45 @@ CREATE TABLE `truckimages` (
 --
 
 INSERT INTO `truckimages` (`image_id`, `truck_id`, `image_path`, `created_at`) VALUES
-(1, 2, 'uploads/images/qay4hh9g.png', '2024-09-29 15:25:25'),
-(2, 3, 'uploads/images/images.jfif', '2024-09-29 15:32:18'),
-(3, 4, 'uploads/images/Screenshot 2024-08-05 195249.png', '2024-09-29 15:41:43'),
-(4, 5, 'uploads/images/qay4hh9g.png', '2024-09-29 15:49:04'),
-(5, 6, 'uploads/images/qay4hh9g.png', '2024-09-29 15:50:59'),
-(6, 7, 'uploads/images/agriauction.png', '2024-09-29 15:54:03');
+(1, 1, 'uploads/images/agriauction.png', '2024-10-01 05:03:36'),
+(2, 1, 'uploads/images/agriauction logo.png', '2024-10-01 05:03:36'),
+(3, 1, 'uploads/images/Screenshot 2024-08-05 201446.png', '2024-10-01 05:03:36'),
+(4, 2, 'uploads/images/agriauction.png', '2024-10-01 05:04:01'),
+(5, 3, 'uploads/images/Week 3 (Techno).png', '2024-10-01 07:00:21'),
+(6, 4, 'uploads/images/Week 3 (Techno).png', '2024-10-01 12:22:19'),
+(7, 5, 'uploads/images/agriauction.png', '2024-10-01 12:29:25'),
+(8, 5, 'uploads/images/agriauction logo.png', '2024-10-01 12:29:25'),
+(9, 5, 'uploads/images/Screenshot 2024-08-05 201446.png', '2024-10-01 12:29:25'),
+(10, 6, 'uploads/images/logo.png', '2024-10-01 12:43:30'),
+(11, 7, 'uploads/images/Week 3 (Techno).png', '2024-10-01 13:58:41'),
+(12, 7, 'uploads/images/Green and Yellow Leaf Icon Agriculture Logo.png', '2024-10-01 13:58:41'),
+(13, 7, 'uploads/images/image.png', '2024-10-01 13:58:41'),
+(14, 7, 'uploads/images/Untitled design.png', '2024-10-01 13:58:41'),
+(15, 7, 'uploads/images/jmalogo-removebg-preview.png', '2024-10-01 13:58:41'),
+(16, 10, 'uploads/images/agriauction.png', '2024-10-03 01:03:16'),
+(17, 10, 'uploads/images/Screenshot 2024-08-05 201446.png', '2024-10-03 01:03:16');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `truckmedia`
+-- Table structure for table `truckmakers`
 --
 
-CREATE TABLE `truckmedia` (
-  `media_id` int(11) NOT NULL,
-  `truck_id` int(11) DEFAULT NULL,
-  `media_url` varchar(255) NOT NULL,
-  `media_type` enum('image','video') NOT NULL,
-  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+CREATE TABLE `truckmakers` (
+  `maker_id` int(11) NOT NULL,
+  `maker_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `truckmakers`
+--
+
+INSERT INTO `truckmakers` (`maker_id`, `maker_name`, `created_at`) VALUES
+(1, 'Hino', '2024-10-03 00:56:41'),
+(2, 'Isuzu', '2024-10-03 00:56:41'),
+(3, 'Mitsubishi', '2024-10-03 00:56:41'),
+(4, 'Fuso', '2024-10-03 00:56:41');
 
 -- --------------------------------------------------------
 
@@ -210,21 +227,25 @@ CREATE TABLE `trucks` (
   `auction_end` datetime NOT NULL,
   `status` enum('active','sold','canceled') DEFAULT 'active',
   `category_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `maker_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `trucks`
 --
 
-INSERT INTO `trucks` (`truck_id`, `truck_name`, `description`, `model_year`, `starting_bid`, `current_bid`, `auction_start`, `auction_end`, `status`, `category_id`, `created_at`) VALUES
-(1, 'dump truck', 'g', 1, 675675.00, 0.00, '2024-09-29 23:24:00', '2024-09-30 23:24:00', 'active', 12, '2024-09-29 15:24:31'),
-(2, 'dump truck', '312', 321, 321321.00, 0.00, '2024-09-29 23:25:00', '2024-09-30 23:25:00', 'active', 17, '2024-09-29 15:25:25'),
-(3, 'qweqwewq', 'ewqewq', 2131, 321312.00, 0.00, '2024-09-29 23:31:00', '2024-09-30 23:31:00', 'active', 16, '2024-09-29 15:32:18'),
-(4, 'fsdfsd', 'fsdfsd', 32432, 432432.00, 0.00, '2024-09-29 23:41:00', '2024-09-30 23:41:00', 'active', 12, '2024-09-29 15:41:43'),
-(5, 'wqewqeqw', 'ewqewq', 321312, 321.00, 0.00, '2024-09-29 23:44:00', '2024-09-30 23:44:00', 'active', 12, '2024-09-29 15:49:04'),
-(6, 'dasdas', 'dasdas', 3213, 321321.00, 0.00, '2024-09-29 23:50:00', '2024-09-30 23:50:00', 'active', 16, '2024-09-29 15:50:59'),
-(7, '3432432', '432432', 432, 432.00, 0.00, '2024-09-29 23:53:00', '2024-09-30 23:53:00', 'active', 11, '2024-09-29 15:54:03');
+INSERT INTO `trucks` (`truck_id`, `truck_name`, `description`, `model_year`, `starting_bid`, `current_bid`, `auction_start`, `auction_end`, `status`, `category_id`, `created_at`, `maker_id`) VALUES
+(1, 'dump truck', 'dsadas', 2321, 321312.00, 500000.00, '2024-10-01 13:03:00', '2024-10-02 13:03:00', 'active', 12, '2024-10-01 05:03:36', NULL),
+(2, 'dump truck', 'fgfsdfsd', 432432, 432432.00, 0.00, '2024-10-01 13:03:00', '2024-10-02 13:03:00', 'active', 14, '2024-10-01 05:04:01', NULL),
+(3, 'dd', 'dd', 33, 33.00, 0.00, '2024-10-01 14:59:00', '2024-10-02 14:59:00', 'active', 8, '2024-10-01 07:00:21', NULL),
+(4, 'vcxvxc', 'xcvxcvxc', 342, 432.00, 0.00, '2024-10-01 20:22:00', '2024-10-02 20:22:00', 'active', 16, '2024-10-01 12:22:19', NULL),
+(5, 'ff', 'ff', 312, 523.00, 0.00, '2024-10-01 20:29:00', '2024-10-02 20:29:00', 'active', 14, '2024-10-01 12:29:25', NULL),
+(6, 'dsads', 'dasdas', 3123, 2321312.00, 0.00, '2024-10-02 20:43:00', '2024-09-30 20:43:00', 'active', 14, '2024-10-01 12:43:30', NULL),
+(7, 'dump truck', 'dsdsad', 2015, 100000.00, 0.00, '2024-10-01 21:58:00', '2024-10-02 21:58:00', 'active', 14, '2024-10-01 13:58:41', NULL),
+(8, 'dasdas', '321312', 321312, 312312.00, 0.00, '2024-10-03 09:02:00', '2024-10-04 09:02:00', 'active', 16, '2024-10-03 01:02:19', 2),
+(9, 'dasdas', '321', 321, 321.00, 0.00, '2024-10-03 09:02:00', '2024-10-04 09:02:00', 'active', 15, '2024-10-03 01:02:50', 1),
+(10, 'sadas', 'das', 2131, 21312.00, 50.00, '2024-10-03 09:02:00', '2024-10-04 09:03:00', 'active', 17, '2024-10-03 01:03:16', 1);
 
 -- --------------------------------------------------------
 
@@ -244,12 +265,24 @@ CREATE TABLE `truckvideos` (
 --
 
 INSERT INTO `truckvideos` (`video_id`, `truck_id`, `video_path`, `created_at`) VALUES
-(1, 2, 'uploads/videos/what-is-cybersecurity.jpg', '2024-09-29 15:25:25'),
-(2, 3, 'uploads/videos/2023-11-30 13-14-27.mp4', '2024-09-29 15:32:18'),
-(3, 4, 'uploads/videos/2023-10-27 14-33-49.mp4', '2024-09-29 15:41:43'),
-(4, 5, 'uploads/videos/The 200 Attack of Mafiaboy PPT.mp4', '2024-09-29 15:49:04'),
-(5, 6, 'uploads/videos/2023-11-17 20-21-24.mp4', '2024-09-29 15:50:59'),
-(6, 7, 'uploads/videos/The 200 Attack of Mafiaboy PPT.mp4', '2024-09-29 15:54:03');
+(1, 1, 'uploads/videos/2023-11-30 12-46-18.mp4', '2024-10-01 05:03:36'),
+(2, 1, 'uploads/videos/2023-11-30 12-58-36.mp4', '2024-10-01 05:03:36'),
+(3, 2, 'uploads/videos/2023-10-24 17-21-10.mp4', '2024-10-01 05:04:01'),
+(4, 3, 'uploads/videos/Untitled design.mp4', '2024-10-01 07:00:21'),
+(5, 4, 'uploads/videos/2023-10-24 17-23-32.mp4', '2024-10-01 12:22:19'),
+(6, 5, 'uploads/videos/2023-11-30 12-46-18.mp4', '2024-10-01 12:29:25'),
+(7, 5, 'uploads/videos/2023-11-30 12-58-36.mp4', '2024-10-01 12:29:25'),
+(8, 5, 'uploads/videos/2023-10-24 17-23-32.mp4', '2024-10-01 12:29:25'),
+(9, 6, 'uploads/videos/2023-11-30 13-02-58.mp4', '2024-10-01 12:43:30'),
+(10, 7, 'uploads/videos/2023-11-30 13-04-04.mp4', '2024-10-01 13:58:41'),
+(11, 7, 'uploads/videos/2023-11-30 13-02-58.mp4', '2024-10-01 13:58:41'),
+(12, 7, 'uploads/videos/2023-11-17 20-03-38.mp4', '2024-10-01 13:58:41'),
+(13, 7, 'uploads/videos/2023-11-30 12-46-18.mp4', '2024-10-01 13:58:41'),
+(14, 7, 'uploads/videos/2023-11-30 12-58-36.mp4', '2024-10-01 13:58:41'),
+(15, 7, 'uploads/videos/2023-10-24 17-21-10.mp4', '2024-10-01 13:58:41'),
+(16, 7, 'uploads/videos/2023-11-30 13-03-15.mp4', '2024-10-01 13:58:41'),
+(17, 10, 'uploads/videos/2023-11-30 12-58-36.mp4', '2024-10-03 01:03:16'),
+(18, 10, 'uploads/videos/2023-10-24 17-23-32.mp4', '2024-10-03 01:03:16');
 
 -- --------------------------------------------------------
 
@@ -264,35 +297,32 @@ CREATE TABLE `userreviews` (
   `rating` int(11) DEFAULT NULL CHECK (`rating` >= 1 and `rating` <= 5),
   `review_text` text DEFAULT NULL,
   `review_time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `image` varchar(255) DEFAULT NULL
+  `images` longtext DEFAULT NULL,
+  `videos` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `userreviews`
 --
 
-INSERT INTO `userreviews` (`review_id`, `transaction_id`, `username`, `rating`, `review_text`, `review_time`, `image`) VALUES
-(21, 2, 'c', 3, 'hindi sya ok ', '2024-09-20 09:01:05', ''),
-(22, 4, 'c', 4, 'dhfhy', '2024-09-20 09:01:28', 'IPR-protection-ideas-801x684.png'),
-(23, 0, 'c', 1, 'lgl', '2024-09-20 09:24:36', '281867236_763037858403949_2455834457936504847_n-removebg-preview.png'),
-(24, 0, 'c', 5, 'pang ilan n atah arehehh', '2024-09-20 09:26:16', ''),
-(25, 0, 'c', 3, 'ewgeg', '2024-09-20 09:26:31', ''),
-(26, 0, 'c', 3, 'jgkhifoi', '2024-09-20 09:27:20', 'images.jfif'),
-(27, 3, 'c', 4, 'sgsg', '2024-09-20 09:28:02', 'what-is-cybersecurity.jpg'),
-(28, NULL, 'c', NULL, NULL, '2024-09-20 09:30:32', NULL),
-(29, 0, 'c', 1, 'hhh', '2024-09-20 09:31:38', 'Untitled design.mp4'),
-(30, 0, 'c', 1, 'zzz', '2024-09-20 09:33:48', 'Green and Yellow Leaf Icon Agriculture Logo.png'),
-(31, 1, 'c', 1, '3333', '2024-09-20 09:45:19', ''),
-(32, 2, 'c', 2, '333', '2024-09-20 09:45:29', ''),
-(33, 0, 'c', 2, 'ee', '2024-09-20 09:46:03', ''),
-(34, 0, 'c', 2, 'eqweq', '2024-09-20 09:46:21', 'Green and Yellow Leaf Icon Agriculture Logo.png'),
-(35, 0, 'c', 2, 'uuu', '2024-09-20 09:46:35', ''),
-(36, 1, 'c', 3, 'qweqw', '2024-09-20 09:47:14', ''),
-(37, 0, 'c', 3, 'eeee', '2024-09-20 09:48:15', ''),
-(38, 0, 'c', 4, 'kkkk', '2024-09-20 09:48:31', 'jmalogo-removebg-preview.png'),
-(39, 0, 'c', 3, 'bb', '2024-09-20 09:48:44', 'Week 3 (Techno).png'),
-(40, 0, '1', 1, 'vv', '2024-09-20 09:51:13', ''),
-(41, 1, '1', 2, '5', '2024-09-23 15:30:20', '');
+INSERT INTO `userreviews` (`review_id`, `transaction_id`, `username`, `rating`, `review_text`, `review_time`, `images`, `videos`) VALUES
+(1, 2, '1', 5, 'all is good ', '2024-09-30 13:53:06', '[\"66faad42b8a1b_agriauction.png\",\"66faad42b91f1_agriauctionlogo.png\",\"66faad42b9a85_Screenshot2024-08-05201446.png\"]', '[\"66faad42ba220_2023-11-3012-58-36.mp4\"]'),
+(2, 1, 'c', 5, 'fdsfdsfsd', '2024-10-02 10:25:01', '[\"66fd1f7db984c_agriauction.png\",\"66fd1f7dba036_agriauctionlogo.png\"]', '[\"66fd1f7dba5ab_2023-11-3013-04-04.mp4\",\"66fd1f7dbaa65_2023-11-3013-02-58.mp4\"]'),
+(3, 1, 'c', 5, 'sss', '2024-10-02 16:10:22', '[]', '[]'),
+(4, 1, 'c', 5, 'sss', '2024-10-02 16:10:26', '[]', '[]'),
+(5, 1, 'c', 5, 'sss', '2024-10-02 16:10:29', '[]', '[]'),
+(6, 1, 'c', 5, 'sss', '2024-10-02 16:10:32', '[]', '[]'),
+(7, 1, 'c', 5, 'd', '2024-10-02 16:11:36', '[]', '[]'),
+(8, 1, 'c', 5, 'a', '2024-10-02 16:13:52', '[]', '[]'),
+(9, 1, 'c', 5, 'a', '2024-10-02 16:13:58', '[]', '[]'),
+(10, 1, 'c', 5, 's', '2024-10-02 16:14:11', '[]', '[]'),
+(11, 1, 'c', 5, 's', '2024-10-02 16:14:35', '[]', '[]'),
+(12, 1, 'c', 5, 'cccc', '2024-10-02 16:14:45', '[]', '[]'),
+(13, 1, 'c', 5, 'd', '2024-10-02 16:16:58', '[]', '[]'),
+(14, 1, 'c', 5, 'gg', '2024-10-02 16:17:27', '[]', '[]'),
+(15, 1, 'c', 5, 'gg', '2024-10-02 16:17:31', '[]', '[]'),
+(16, 1, 'c', 5, 'gg', '2024-10-02 16:17:48', '[]', '[]'),
+(17, 1, 'c', 5, 'a', '2024-10-02 16:19:09', '[]', '[]');
 
 -- --------------------------------------------------------
 
@@ -325,11 +355,31 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password_hash`, `first_name`, `middle_name`, `last_name`, `contact`, `age`, `address`, `business_permit`, `govt_id`, `created_at`, `email_verified`, `verification_token`, `reset_token`, `token_expiry`) VALUES
-(1, 'c', 'rjtm15703@gmail.com', '$2y$10$pDB0kVNDZepE/mS6hPFEiur6DKruxZ8jR/wi4ISgia/Mfft60J0z.', 'c', 'c', 'c', 'c', '0', 'c', 'uploads/Week 3 (Techno).png', 'business_permit', '2024-09-19 16:22:17', 1, NULL, NULL, NULL),
+(1, 'c', 'rjtm15703@gmail.com', '$2y$10$FJVvVUnlvpXLL84f1Sv5hOFzprUSi5tae3cxKA5QYs4alSWVfW1fW', 'c', 'c', 'c', 'c', '0', 'c', 'uploads/Week 3 (Techno).png', 'business_permit', '2024-09-19 16:22:17', 1, NULL, NULL, NULL),
 (2, '1', 'mryan1669@gmail.com', '$2y$10$P6bdr4D7cZKIy2lHCRYAyu/6nIAJocfKcnRKWu./tLfW6ut.2wppW', '1', '1', '1', '1', '1', '1', 'uploads/Green and Yellow Leaf Icon Agriculture Logo.png', 'business_permit', '2024-09-20 04:08:51', 1, NULL, NULL, '0000-00-00 00:00:00'),
 (3, 'y', 'y@gmail.com', '$2y$10$Ii0I5SNAXQUrD2Li5j0CZu5/dbHHhzl6nQGrZlvWS5yc2uga3TvO2', 'y', 'y', 'y', 'y', '0', 'y', 'uploads/460410173_511831891556364_2203269310027697891_n.jpg', 'dti_permit', '2024-09-26 03:53:22', 1, NULL, NULL, '0000-00-00 00:00:00'),
 (9, 'g', 'g@mail.com', '$2y$10$UdguvjW.e8IY.ueQgdQG6enw0LcEIBz0.0ligY.YD2dVVO/c40hzm', 'g', 'g', 'g', 'g', '0', 'g', 'uploads/460410173_511831891556364_2203269310027697891_n.jpg', 'business_permit', '2024-09-26 04:36:32', 1, NULL, NULL, '0000-00-00 00:00:00'),
 (17, 'l', 'l@gmail.com', '$2y$10$bFk0qCvM3gWbgPMeZMcLheUD4Bh7NHrVC.mi15tB8W0/XCk0k7lGu', 'l', 'l', 'l', 'l', '0', 'l', NULL, 'dti_permit', '2024-09-27 12:07:06', 1, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `watchlist`
+--
+
+CREATE TABLE `watchlist` (
+  `watchlist_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `truck_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `watchlist`
+--
+
+INSERT INTO `watchlist` (`watchlist_id`, `user_id`, `truck_id`, `created_at`) VALUES
+(1, 17, 1, '2024-10-04 12:12:27');
 
 --
 -- Indexes for dumped tables
@@ -374,13 +424,6 @@ ALTER TABLE `transactions`
   ADD KEY `buyer_id` (`buyer_id`);
 
 --
--- Indexes for table `truckcategories`
---
-ALTER TABLE `truckcategories`
-  ADD PRIMARY KEY (`category_id`),
-  ADD UNIQUE KEY `category_name` (`category_name`);
-
---
 -- Indexes for table `truckcategorymapping`
 --
 ALTER TABLE `truckcategorymapping`
@@ -395,11 +438,10 @@ ALTER TABLE `truckimages`
   ADD KEY `truck_id` (`truck_id`);
 
 --
--- Indexes for table `truckmedia`
+-- Indexes for table `truckmakers`
 --
-ALTER TABLE `truckmedia`
-  ADD PRIMARY KEY (`media_id`),
-  ADD KEY `truck_id` (`truck_id`);
+ALTER TABLE `truckmakers`
+  ADD PRIMARY KEY (`maker_id`);
 
 --
 -- Indexes for table `trucks`
@@ -431,6 +473,14 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `watchlist`
+--
+ALTER TABLE `watchlist`
+  ADD PRIMARY KEY (`watchlist_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `truck_id` (`truck_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -456,55 +506,55 @@ ALTER TABLE `bids`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `truckcategories`
---
-ALTER TABLE `truckcategories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `truckimages`
 --
 ALTER TABLE `truckimages`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `truckmedia`
+-- AUTO_INCREMENT for table `truckmakers`
 --
-ALTER TABLE `truckmedia`
-  MODIFY `media_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `truckmakers`
+  MODIFY `maker_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `trucks`
 --
 ALTER TABLE `trucks`
-  MODIFY `truck_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `truck_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `truckvideos`
 --
 ALTER TABLE `truckvideos`
-  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `userreviews`
 --
 ALTER TABLE `userreviews`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `watchlist`
+--
+ALTER TABLE `watchlist`
+  MODIFY `watchlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -544,12 +594,6 @@ ALTER TABLE `truckimages`
   ADD CONSTRAINT `truckimages_ibfk_1` FOREIGN KEY (`truck_id`) REFERENCES `trucks` (`truck_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `truckmedia`
---
-ALTER TABLE `truckmedia`
-  ADD CONSTRAINT `truckmedia_ibfk_1` FOREIGN KEY (`truck_id`) REFERENCES `trucks` (`truck_id`) ON DELETE CASCADE;
-
---
 -- Constraints for table `trucks`
 --
 ALTER TABLE `trucks`
@@ -566,6 +610,13 @@ ALTER TABLE `truckvideos`
 --
 ALTER TABLE `userreviews`
   ADD CONSTRAINT `userreviews_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `watchlist`
+--
+ALTER TABLE `watchlist`
+  ADD CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `watchlist_ibfk_2` FOREIGN KEY (`truck_id`) REFERENCES `trucks` (`truck_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
